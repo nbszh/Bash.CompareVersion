b95a1405952e11cd5ff4526214464f619e817764
NZ <nzhang@rdmcorp.com>
2015-10-28 14:42:35 -0400

    Update version compare policy
    1. if both are number, they are compared base on numberic values
    2. if either or both are not numbers
       2.1 if they have the same string length, they are compared based on ASCII order
       2.2 if they have different length:
       2.2.1 if the short one starts with a digit, prepend 0s to the short one to the lenght of the other string, and compared based on ASCII order
       2.2.2 otherwise compared based on ASCII order

--- CompareVersions.sh
+++ CompareVersions.sh
@@ -1,19 +1,49 @@
 #!/bin/sh
 
-VER_SPLIT_SED="s/[\.\-_]/:/g;s/\([[:digit:]]\)\([^0-9:]\)/\1:\2/g;s/\([^0-9:]\)\([[:digit:]]\)/\1\:\2/g;"
+VER_SPLIT_SED="s/\-/:/g;s/_/:/g;s/\./:/g"
 
 # Compare with one segment of versions
 ver_cmp_seg() {
+  local s1 s2 prefixlen
   [[ "$1" == "$2" ]] && return 0
-  if [[ -z "${1//[[:digit:]]/}" ]] && [[ -z "${2//[[:digit:]]/}" ]]; then
+  if [[ -n "$1" ]] && [[ -n "$2" ]] && [[ -z "${1//[[:digit:]]/}" ]] && [[ -z "${2//[[:digit:]]/}" ]]; then
     # "Both $1 and $2 are numbers"
     [[ "$1" -gt "$2" ]] && return 1
     [[ "$1" -lt "$2" ]] && return 2
-  else
-    # "Either or both of '$1' '$2' are not numbers"
-    [[ "$1" > "$2" ]] && return 1
-    [[ "$1" < "$2" ]] && return 2
+    return 0
   fi
+  
+  # "Either or both of '$1' '$2' are not numbers"
+  if [[ "${#1}" -gt "${#2}" ]]; then
+    prefixlen=$((${#1} - ${#2}))
+    s1="$1"
+    case "$2" in
+    [0-9]*)
+      s2="$(printf %0${prefixlen}d 0)${2}"
+    ;;
+    *)
+      s2="$2"
+    ;;
+    esac
+  elif [[ "${#1}" -lt "${#2}" ]]; then
+    prefixlen=$((${#2} - ${#1}))
+    s2="$2"
+    case "$1" in
+    [0-9]*)
+      s1="$(printf %0${prefixlen}d 0)${1}"
+    ;;
+    *)
+      s1="$1"
+    ;;
+    esac
+  else
+    s1="$1"
+    s2="$2"
+  fi
+      
+  [[ "$1" \> "$2" ]] && return 1
+  [[ "$1" \< "$2" ]] && return 2
+
   return 0
 }
 
@@ -30,7 +60,7 @@ ver_cmp() {
     [[ "x$result" != "x0" ]] && return $result
 
     tmp=${ver1#*:}
-    if [ $tmp == $ver1 ]
+    if [[ $tmp == $ver1 ]]
     then
       ver1=''
     else
@@ -38,14 +68,14 @@ ver_cmp() {
     fi
 
     tmp=${ver2#*:}
-    if [ $tmp == $ver2 ]
+    if [[ $tmp == $ver2 ]]
     then
       ver2=''
     else
       ver2=$tmp
     fi
 
-    if [ -z "$ver1" ] || [ -z "$ver2" ]
+    if [[ -z "$ver1" ]] || [[ -z "$ver2" ]]
     then
       break
     fi
